6351417e98f2f21c83700285c3f699c9cd47398d
pakaratAriya <pakarat_ariya@hotmail.com>
2019-11-05 20:30:09 -0800

    fix db config

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 // connect to a database peoplebook running on your localmachine
 const pool = mysql.createPool({
-    host: 'heroku_25164e32b44a522',
+    host: 'us-cdbr-iron-east-05',
     user: 'b734bf2b717e37',
     database: 'artistBook',
     password: '90b45c4a'
