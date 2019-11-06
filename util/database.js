9e31a77c07d0c6813660f91cb1b095231cf657f3
pakaratAriya <pakarat_ariya@hotmail.com>
2019-11-05 21:41:57 -0800

    asd

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 // connect to a database peoplebook running on your localmachine
 const pool = mysql.createPool({
-    host: 'us-cdbr-iron-east-05',
+    host: 'us-cdbr-iron-east-05.cleardb.net',
     user: 'b734bf2b717e37',
     database: 'heroku_25164e32b44a522',
     password: '90b45c4a'
