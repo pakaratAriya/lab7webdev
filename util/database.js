ab68a45d8aea1c3ac405e873c065019a43ac372d
pakaratAriya <pakarat_ariya@hotmail.com>
2019-11-05 20:23:27 -0800

    change database

--- util/database.js
+++ util/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // connect to a database peoplebook running on your localmachine
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
+    host: 'heroku_25164e32b44a522',
+    user: 'b734bf2b717e37',
     database: 'artistBook',
-    password: ''
+    password: '90b45c4a'
 });
 
 module.exports = pool.promise();
